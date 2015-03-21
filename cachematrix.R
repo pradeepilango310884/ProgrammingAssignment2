d7983a31e011e43f79e561024c84a10494ab4acc
pradeepilango310884 <pradeepilango@yahoo.co.in>
2015-03-21 22:51:04 +0530

    #The first function, makeCacheMatrix creates a special "vector", which is really a list containing a function to #set the value of the matrix #get the value of the matrix #set the value of the inverse of matrix #get the value of the inverse of matrix
    makeCacheMatrix <- function(x = matrix()) {
      inv <- NULL
      set <- function(y) {
        x <<- y
        inv <<- NULL
      }
      get <- function() x
      setinverse <- function(inverse) inv <<- inverse
      getinverse <- function() inv
      list(
        set = set,
        get = get,
        setinverse = setinverse,
        getinverse = getinverse
      )
    }
    
    ## cachesolve Calculates the inverse of the special "matrix" created with the above
    ## function, and it reuses cached result if it is available else calculates freshly
    
    cacheSolve <- function(x, ...) {
      inv <- x$getinverse()
      if(!is.null(inv)) {
        message("getting cached data")
        return(inv)
      }
      mat <- x$get()
      inv <- solve(mat, ...)
      x$setinverse(inv)
      return (inv)
    }

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+#The first function, makeCacheMatrix creates a special "vector", which is really a list containing a function to
+#set the value of the matrix
+#get the value of the matrix
+#set the value of the inverse of matrix
+#get the value of the inverse of matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(
+    set = set,
+    get = get,
+    setinverse = setinverse,
+    getinverse = getinverse
+  )
 }
 
 
-## Write a short comment describing this function
+## cachesolve Calculates the inverse of the special "matrix" created with the above
+## function, and it reuses cached result if it is available else calculates freshly
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setinverse(inv)
+  return (inv)
 }
